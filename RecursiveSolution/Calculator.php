8cb443b1dd746b24a31766228ec751b5ace12f74
Alles Alexey <aalles@torbor.ru>
2021-07-20 23:02:45 +0300

    feat: add recursion and sort solution

---
+++ RecursiveSolution/Calculator.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Alleswebdev\RecursiveSolution;
+
+/**
+ * Так как в постановке ожидается линейная сложность, найдём медиану с помощью рекурсии.
+ * Вероятно, в мире есть более оптимальные способы решения, но без гугла мне в голову пришел этот.
+ * Class Calculator
+ * @package Alleswebdev\RecursiveSolution
+ */
+class Calculator
+{
+    public static function calculateLocation(array $houses):float
+    {
+        return static::recursiveCalculation($houses);
+    }
+
+    /**
+     * Рекурсивное нахождение медианы в заданном числовом массиве неотсортированных целых чисел
+     * Средняя сложность O(N)
+     * @param array $houses
+     * @param int|null $pivot опорный элемент, рандомный индекс из $houses
+     * @param int $iter счетчик итераций рекурсии, бесполезен
+     * @return float
+     */
+    protected static function recursiveCalculation(array $houses, ?int $pivot = null, int $iter = 0): float
+    {
+        if(!count($houses)){
+            throw new \BadFunctionCallException("THe count of $houses cann't be zero!");
+        }
+
+        $totalCount = count($houses);
+
+        if ($iter > $totalCount) {
+            echo "Сложность решения нелинейна :(";
+            die();
+        }
+
+        if($totalCount == 2){
+            return array_sum($houses) / 2;
+        }
+
+        $pivot = $pivot ?? $houses[array_rand($houses)];
+
+        $lessCount = 0;
+        $greaterCount = 0;
+        $pivotCount = 0;
+
+        $minGreater = null;
+        $maxLess = null;
+
+        foreach ($houses as $dist) {
+            if ($dist === $pivot) {
+                $pivotCount++;
+                continue;
+            }
+
+            if ($dist < $pivot) {
+                $lessCount++;
+                if ($dist > $maxLess) {
+                    $maxLess = $dist;
+                }
+                continue;
+            }
+
+            if (!$minGreater) {
+                $minGreater = $dist;
+            }
+
+            $greaterCount++;
+            if ($dist < $minGreater) {
+                $minGreater = $dist;
+            }
+        }
+
+        // чудо
+        if ($lessCount === $greaterCount) {
+            return $pivot;
+        }
+
+        // нашли медиану в чётных
+        if (($lessCount + $pivotCount) == $greaterCount) {
+            return ($minGreater + $pivot) / 2;
+        }
+
+        // в нечётных повторяется опорный, но опорный является медианой
+        if ((($greaterCount + $pivotCount) > $lessCount) && ($lessCount > $greaterCount)) {
+            return $pivot;
+        }
+
+        if ((($lessCount + $pivotCount) > $greaterCount) && ($lessCount < $greaterCount)) {
+            return $pivot;
+        }
+
+        if ($lessCount > $greaterCount) {
+            return static::recursiveCalculation($houses, $maxLess ?? 0, ++$iter);
+        }
+
+        return static::recursiveCalculation($houses, $minGreater, ++$iter);
+    }
+}
---
+++ SortSolution/Calculator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Alleswebdev\SortSolution;
+
+/**
+ * В постановке задачи нет никаких запретов на сортировку (не считая ожидаемой сложности),
+ * самый быстрый способ найти медиану это брать середину в отсортированном массиве.
+ * В таком случае сложность зависит только от сложности сортировки.
+ * Средняя сложность O(log n)
+ * Class Calculator
+ * @package Alleswebdev\SortSolution
+ */
+class Calculator
+{
+    public static function calculateLocation(array $houses):float
+    {
+        static::quicksort($houses);
+
+        if ((count($houses) % 2)) {
+            return $houses[((count($houses) / 2))];
+        }
+
+        return ($houses[((count($houses) / 2) - 1)] + $houses[((count($houses) / 2))]) * 0.5;
+    }
+
+    /**
+     * Реализуем quicksort или возьмём стандартный sort()
+     * @param $array
+     * @param int $l
+     * @param int $r
+     */
+    protected static function quicksort(&$array, int $l = 0, int $r = 0)
+    {
+        if (!$r) {
+            $r = count($array) - 1;
+        }
+
+        $i = $l;
+        $j = $r;
+
+        $x = $array[($l + $r) / 2];
+
+        do {
+            while ($array[$i] < $x) {
+                $i++;
+            }
+            while ($array[$j] > $x) {
+                $j--;
+            }
+            if ($i <= $j) {
+                if ($array[$i] > $array[$j]) {
+                    list($array[$i], $array[$j]) = [$array[$j], $array[$i]];
+                }
+                $i++;
+                $j--;
+            }
+        } while ($i <= $j);
+        if ($i < $r) {
+            static::quicksort($array, $i, $r);
+        }
+        if ($j > $l) {
+            static::quicksort($array, $l, $j);
+        }
+    }
+}
